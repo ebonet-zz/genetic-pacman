5f6945ad0c1acdc5eafd4dadb6b98c65f64c3fd2
caiovgb@gmail.com <caiovgb@gmail.com@0c4c7884-f1b2-7e1a-34d0-f963ce89a309>
2012-10-25 04:20:16 +0000

    Ultimate winning agent

--- trunk/genetic-pacman/myTeam.py
+++ trunk/genetic-pacman/myTeam.py
@@ -89,125 +89,6 @@ class DummyAgent(CaptureAgent):
 ##################################
 #           My Team              #
 ##################################
-    
-class AttackAgent(CaptureAgent):
-    """
-    You should look at baselineTeam.py for more details about how to
-    """
-
-    def registerInitialState(self, gameState):
-        """
-        This method handles the initial setup of the
-        agent to populate useful fields (such as what team
-        we're on). 
-    
-        A distanceCalculator instance caches the maze distances
-        between each pair of positions, so your agents can use:
-        self.distancer.getDistance(p1, p2)
-
-        IMPORTANT: This method may run for at most 15 seconds.
-        """
-
-        ''' 
-        Make sure you do not delete the following line. If you would like to
-        use Manhattan distances instead of maze distances in order to save
-        on initialization time, please take a look at
-        CaptureAgent.registerInitialState in captureAgents.py. 
-        '''
-        CaptureAgent.registerInitialState(self, gameState)
-
-        ''' 
-        Your initialization code goes here, if you need any.
-        '''
-
-    def chooseAction(self, gameState):
-      
-        actions = gameState.getLegalActions(self.index)
-        # actions = [a for a in actions if a != Directions.STOP]
-    
-        previousPos = gameState.getAgentState(self.index).getPosition()
-
-        foodGrid = self.getFood(gameState)
-        foodList = foodGrid.asList()
-        capsules = self.getCapsules(gameState)
-      
-        allies = [gameState.getAgentState(i) for i in self.getTeam(gameState)]
-        hunters = [a for a in allies if a.isPacman and a.getPosition() != None]
-        # defenders = [a for a in allies if not a.isPacman and a.getPosition() != None]
-        
-        enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
-        # invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
-        ghosts = [a for a in enemies if not a.isPacman and a.getPosition() != None]
-      
-        evalFunc = self.generateEvalFunc(self.generateOffensiveGaussians(foodList, foodGrid, capsules, hunters, ghosts))
-    
-        possibleCells = [self.getActionCoordinates(action, previousPos) for action in actions]
-        actionPoints = [evalFunc(cell) for cell in possibleCells]
-        
-        if not actionPoints:
-            return Directions.STOP
-    
-        maxValue = max(actionPoints)
-
-#        for i in range(len(possibleCells)):
-#            self.debugDraw(possibleCells[i], [actionPoints[i] / 60, 0, 0] if actionPoints[i] > 0 else [0, -actionPoints[i] / 250, 0], False) 
-        
-        bestActions = [a for a, v in zip(actions, actionPoints) if v == maxValue]
-
-        return random.choice(bestActions)
-    
-    def getActionCoordinates(self, action, previousCoordinates):
-        dx, dy = Actions.directionToVector(action)
-        return (previousCoordinates[0] + dx, previousCoordinates[1] + dy)
-
-    def generateOffensiveGaussians(self, foodList, foodGrid, capsules, hunters, ghosts):
-        # def cellEvaluation(coordinates):
-        #    return coordinates[0] + coordinates[1]
-      
-        self.chromoawesome = [20.0, 1.0, -150.0, 0.8, 200.0, 0.5, -5, 0.5, 22.0, 0.8, 100.0, 0.3]
-        gaussians = []
-      
-        for food in foodList:
-            gaussians.append(self.gaussian(self.chromoawesome[0], self.chromoawesome[1], food[0], food[1]))
-            
-            isLonely = True
-            for i in [-1, 0, 1]:
-                for j in [-1, 0, 1]:
-                    isCellInGrid = (food[0] + i < foodGrid.width) and (food[0] + i >= 0) and (food[1] + j < foodGrid.height) and (food[1] + j) >= 0
-                    if (i != 0 or j != 0) and isCellInGrid and foodGrid[food[0] + i][food[1] + j]:
-                        isLonely = False
-                        break
-            
-            if isLonely:
-                gaussians.append(self.gaussian(self.chromoawesome[10], self.chromoawesome[11], food[0], food[1]))                            
-            
-        for capsule in capsules:
-            gaussians.append(self.gaussian(self.chromoawesome[8], self.chromoawesome[9], capsule[0], capsule[1]))
-      
-        for ghost in ghosts:
-            if ghost.scaredTimer == 0:
-                gaussians.append(self.gaussian(self.chromoawesome[2], self.chromoawesome[3], ghost.getPosition()[0], ghost.getPosition()[1]))
-            else:
-                gaussians.append(self.gaussian(self.chromoawesome[4], self.chromoawesome[5], ghost.getPosition()[0], ghost.getPosition()[1]))
-                
-        for hunter in hunters:
-            gaussians.append(self.gaussian(self.chromoawesome[6], self.chromoawesome[7], hunter.getPosition()[0], hunter.getPosition()[1]))
-              
-        return gaussians
-
-    def generateEvalFunc(self, gaussians):
-        def evalC(coordinate):
-            return sumGaussians(coordinate[0], coordinate[1], gaussians)
-        return evalC;
-      
-    def gaussian(self, A, sigma, x0, y0):
-        def gaussianFunc(x, y):
-            distance = self.getMazeDistance((x, y), (x0, y0))
-            return A * exp(-(distance) / (2.0 * sq(sigma)))
-        return gaussianFunc
-    
-    def setChromosome(self, chromosome):
-        self.chromoawesome = chromosome
         
 class DoubleAgent(CaptureAgent):
 
@@ -235,23 +116,26 @@ class DoubleAgent(CaptureAgent):
         ''' 
         Your initialization code goes here, if you need any.
         '''
+        self.chromoawesome = [20.0, 1.0, -150.0, 0.8, 200.0, 0.5, -5, 0.5, 22.0, 0.8, 100.0, 0.3,
+                         0.0, 0.0001, -150.0, 0.8, 300.0, 0.8, 0.0, 0.0001, 0.0, 0.0001, 0.8]
 
     def chooseAction(self, gameState):
         
-        actions = gameState.getLegalActions(self.index)
-        # actions = [a for a in actions if a != Directions.STOP]
+        # actions = gameState.getLegalActions(self.index)
+        actions = [a for a in gameState.getLegalActions(self.index) if a != Directions.STOP]
     
         previousPos = gameState.getAgentState(self.index).getPosition()
 
         foodGrid = self.getFood(gameState)
         foodList = foodGrid.asList()
         capsules = self.getCapsules(gameState)
+        wallsGrid = gameState.getWalls()
         
         defenseFoodGrid = self.getFoodYouAreDefending(gameState)
         defenseFoodList = defenseFoodGrid.asList()
         defenseCapsules = self.getCapsulesYouAreDefending(gameState)
       
-        allies = [gameState.getAgentState(i) for i in self.getTeam(gameState) if i != self.index]
+        allies = [gameState.getAgentState(i) for i in self.getTeam(gameState)]
         hunters = [a for a in allies if a.isPacman and a.getPosition() != None]
         defenders = [a for a in allies if not a.isPacman and a.getPosition() != None]
         
@@ -259,16 +143,24 @@ class DoubleAgent(CaptureAgent):
         invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
         ghosts = [a for a in enemies if not a.isPacman and a.getPosition() != None]
       
-        isPacman = gameState.getAgentState(self.index).isPacman
+        # isPacman = gameState.getAgentState(self.index).isPacman
         amScared = gameState.getAgentState(self.index).scaredTimer != 0
         
         defendMode = False
-        if not isPacman:
-            if not defenders:
-                defendMode = True
+        
+        if invaders:
+            minDistance = maxint
+            closestAllied = None
+            for index in self.getTeam(gameState):
+                minInvaderDistance = min([self.getMazeDistance(gameState.getAgentState(index).getPosition(), i.getPosition()) for i in invaders])
+                if minInvaderDistance < minDistance:
+                    minDistance = minInvaderDistance
+                    closestAllied = index
+            if closestAllied == self.index:
+                defendMode = True  
         
         if not defendMode:
-            evalFunc = self.generateEvalFunc(self.generateOffensiveGaussians(foodList, foodGrid, capsules, hunters, ghosts))
+            evalFunc = self.generateEvalFunc(self.generateOffensiveGaussians(wallsGrid, foodList, foodGrid, capsules, hunters, ghosts))
         else:
             evalFunc = self.generateEvalFunc(self.generateDefenseGaussians(amScared, defenseFoodList, defenseCapsules, defenders, invaders))
             
@@ -287,15 +179,14 @@ class DoubleAgent(CaptureAgent):
 
         return random.choice(bestActions)
 
-    def generateOffensiveGaussians(self, foodList, foodGrid, capsules, hunters, ghosts):
+    def generateOffensiveGaussians(self, wallsGrid, foodList, foodGrid, capsules, hunters, ghosts):
         # def cellEvaluation(coordinates):
         #    return coordinates[0] + coordinates[1]
       
-        chromoawesome = [20.0, 1.0, -150.0, 0.8, 200.0, 0.5, -5, 0.5, 22.0, 0.8, 100.0, 0.3]
         gaussians = []
       
         for food in foodList:
-            gaussians.append(self.gaussian(chromoawesome[0], chromoawesome[1], food[0], food[1]))
+            gaussians.append(self.gaussian(self.chromoawesome[0], self.chromoawesome[1], food[0], food[1]))
             
             isLonely = True
             for i in [-1, 0, 1]:
@@ -306,44 +197,56 @@ class DoubleAgent(CaptureAgent):
                         break
             
             if isLonely:
-                gaussians.append(self.gaussian(chromoawesome[10], chromoawesome[11], food[0], food[1]))                            
+                gaussians.append(self.gaussian(self.chromoawesome[10], self.chromoawesome[11], food[0], food[1]))                            
             
         for capsule in capsules:
-            gaussians.append(self.gaussian(chromoawesome[8], chromoawesome[9], capsule[0], capsule[1]))
+            gaussians.append(self.gaussian(self.chromoawesome[8], self.chromoawesome[9], capsule[0], capsule[1]))
       
         for ghost in ghosts:
             if ghost.scaredTimer == 0:
-                gaussians.append(self.gaussian(chromoawesome[2], chromoawesome[3], ghost.getPosition()[0], ghost.getPosition()[1]))
+                ghostGaussian = self.gaussian(self.chromoawesome[2], self.chromoawesome[3], ghost.getPosition()[0], ghost.getPosition()[1])
+                gaussians.append(ghostGaussian)
+                gaussians.append(self.calculateWallPenalty(wallsGrid, ghostGaussian))
             else:
-                gaussians.append(self.gaussian(chromoawesome[4], chromoawesome[5], ghost.getPosition()[0], ghost.getPosition()[1]))
+                gaussians.append(self.gaussian(self.chromoawesome[4], self.chromoawesome[5], ghost.getPosition()[0], ghost.getPosition()[1]))
                 
         for hunter in hunters:
-            gaussians.append(self.gaussian(chromoawesome[6], chromoawesome[7], hunter.getPosition()[0], hunter.getPosition()[1]))
+            gaussians.append(self.gaussian(self.chromoawesome[6], self.chromoawesome[7], hunter.getPosition()[0], hunter.getPosition()[1]))
               
         return gaussians
-
+    
+    def calculateWallPenalty(self, wallsGrid, ghostGaussian):
+        def getWallPenaltyForCell(x, y):
+            numberOfWalls = 0
+            for i, j in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    isCellInGrid = x + i < wallsGrid.width and x + i >= 0 and y + j < wallsGrid.height and y + j >= 0
+                    if isCellInGrid and wallsGrid[int(x + i)][int(y + j)]:
+                        numberOfWalls += 1
+            
+            return numberOfWalls * ghostGaussian(x, y) * self.chromoawesome[22]
+            
+        return getWallPenaltyForCell
+    
     def generateDefenseGaussians(self, amScared, defenseFoodList, defenseCapsules, defenders, invaders):
         # def cellEvaluation(coordinates):
         #    return coordinates[0] + coordinates[1]
       
-        chromoawesome = [20.0, 1.0, -150.0, 0.8, 200.0, 0.5, -5, 0.5, 22.0, 0.8, 100.0, 0.3,
-                         10.0, 1.0, -150.0, 0.8, 300.0, 0.8, -5, 0.5, 22.0, 0.8, 100.0, 0.3]
         gaussians = []
       
         for food in defenseFoodList:
-            gaussians.append(self.gaussian(chromoawesome[12], chromoawesome[13], food[0], food[1]))         
+            gaussians.append(self.gaussian(self.chromoawesome[12], self.chromoawesome[13], food[0], food[1]))         
             
         for capsule in defenseCapsules:
-            gaussians.append(self.gaussian(chromoawesome[20], chromoawesome[21], capsule[0], capsule[1]))
+            gaussians.append(self.gaussian(self.chromoawesome[20], self.chromoawesome[21], capsule[0], capsule[1]))
       
         for invader in invaders:
             if amScared:
-                gaussians.append(self.gaussian(chromoawesome[14], chromoawesome[15], invader.getPosition()[0], invader.getPosition()[1]))
+                gaussians.append(self.gaussian(self.chromoawesome[14], self.chromoawesome[15], invader.getPosition()[0], invader.getPosition()[1]))
             else:
-                gaussians.append(self.gaussian(chromoawesome[16], chromoawesome[17], invader.getPosition()[0], invader.getPosition()[1]))
+                gaussians.append(self.gaussian(self.chromoawesome[16], self.chromoawesome[17], invader.getPosition()[0], invader.getPosition()[1]))
                 
         for defender in defenders:
-            gaussians.append(self.gaussian(chromoawesome[18], chromoawesome[19], defender.getPosition()[0], defender.getPosition()[1]))
+            gaussians.append(self.gaussian(self.chromoawesome[18], self.chromoawesome[19], defender.getPosition()[0], defender.getPosition()[1]))
               
         return gaussians
     
