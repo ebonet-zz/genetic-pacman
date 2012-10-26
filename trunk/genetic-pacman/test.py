1cb22fc8d19d91fa7c721ade351b9bbd03a16357
cherryl2007@gmail.com <cherryl2007@gmail.com@0c4c7884-f1b2-7e1a-34d0-f963ce89a309>
2012-10-26 05:58:41 +0000

    + comments containing the best chromosomes from the trainings

--- trunk/genetic-pacman/test.py
+++ trunk/genetic-pacman/test.py
@@ -1,16 +1,17 @@
 from capture import runGames, readCommand
 def eval_func():
     # arguments for the pacman game
-    argv = ["-r", "myTeam", "-b", "baselineTeam", "-q", "-n", "10"]
+    argv = ["-r", "myTeam", "-b", "baselineTeam", "-Q", "-n", "10"]
     options = readCommand(argv)  # Get game components based on input
     games = runGames(**options)    
 
     scores = [game.state.data.score for game in games];
     
     average = sum(scores) / 10.0;
-
-    return average;
+    maxi = max(scores)
+    mini = min(scores)
+    return [average, maxi, mini]
 
 if __name__ == "__main__":
-    print "Average is: ",
+    print "Avg/Max/Min: ",
     print eval_func() 
